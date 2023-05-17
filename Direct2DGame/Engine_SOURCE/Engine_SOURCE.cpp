e2e422f3b657eb23a906112e905f3def91a97129
KingdomIsGone <125888639+KingdomIsGone@users.noreply.github.com>
2023-05-17 09:14:04 +0900

    23.5.17 firstSetting

---
+++ Direct2DGame/Engine_SOURCE/Engine_SOURCE.cpp
@@ -0,0 +1,10 @@
+// Engine_SOURCE.cpp : Defines the functions for the static library.
+//
+
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnEngineSOURCE()
+{
+
+}
